765ff71779871c714fcf5a081b9e4d9f840a1de4
Thimalee Athalage <Thimalee@Thimalees-MacBook-Air.local>
2019-08-28 23:19:24 -0400

    change expect in routemap

--- scripts/routeMap.js
+++ scripts/routeMap.js
@@ -3,14 +3,15 @@
 var homePg = require('../pages/homePage');
 var routeMapPg = require('../pages/routeMapPage');
 var enums = require('../commonScripts/enums');
+var common = require('../commonScripts/common')
 
 //Search criterias on variables
-var originByCity = enums.Towns.NEWYORK;
-var destinationByCity = enums.ZipCodes.BOSTON;
-//var originInMap = elements(by.xpath(`//h3[contains(text(), '${originByCity}')]`));
-//var destinationInMap = element(by.xpath(`//h3[contains(text(), '${destinationByCity}')]`));
+var originByCity = enums.originCity.NEWYORK;
+var destinationByCity = enums.cityByZipCode.BOSTON;
+var todayDate = common.todayDate();
 var routeMapUrl = 'https://us.megabus.com/journey-planner/map'
-var plannerurl = 'https://us.megabus.com/journey-planner/journeys?originId=123&destinationId=94&departureDate=2019-05-01&preventSearch=true'
+var plannerurl = `https://us.megabus.com/journey-planner/journeys?originId=123&destinationId=94&departureDate=${todayDate}&preventSearch=true`
+var expectLabel = element(by.xpath('//h2[contains(text(),"Book now")]'));
 
 describe('Explore Route Map scenation', function(){
 
@@ -25,13 +26,12 @@ describe('Explore Route Map scenation', function(){
     it('Search trip by City', function(){
         routeMapPg.get();
         routeMapPg.enterTownAsOrigin(originByCity);
-        //expect(originInMap.isPresent()).toBe(true);
         browser.sleep(5000);
         routeMapPg.enterTownAsDestination(destinationByCity);
-       // expect(destinationInMap.isPresent()).toBe(true);
         browser.sleep(5000);
         routeMapPg.clickSearch();
         browser.sleep(5000);
-        expect(browser.getCurrentUrl()).toEqual(plannerurl);
+        expect(expectLabel.isPresent()).toBe(true);
+        //expect(browser.getCurrentUrl()).toEqual(plannerurl);
     });
 });
